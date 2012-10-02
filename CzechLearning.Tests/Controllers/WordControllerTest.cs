c6277a54d8ca5f160ca5d99b712d2624dd11b70e
Mariano Korman <mkormanc@gmail.com>
2012-10-02 16:33:17 +0100

    Added tests, extended TODO

---
+++ CzechLearning.Tests/Controllers/WordControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CzechLearning.Controllers;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using CzechLearning.Models;
+
+namespace CzechLearning.Tests.Controllers
+{
+    [TestClass]
+    public class WordControllerTest
+    {
+        [TestMethod]
+        public void TestWordControllerIndex()
+        {
+            // Arrange
+            var controller = new WordController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Model);
+            Assert.IsInstanceOfType (result.Model, typeof (List<Word>));
+        }
+    }
+}
---
+++ CzechLearning.Tests/Models/WordQuizTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CzechLearning.Models;
+using System.Linq;
+
+namespace CzechLearning.Tests.Models
+{
+    [TestClass]
+    public class WordQuizTest
+    {
+
+        private const string ENGLISH = "SomeEnglishText";
+        private const string CZECH = "SomeCzechText";
+
+        [TestMethod]
+        public void TestWordQuizConstructor()
+        {
+            //Arrange
+            var word = new Word();
+            word.English = ENGLISH;
+            word.Czech = CZECH;
+            var quiz = new WordQuiz(word);
+
+            //Act
+            
+            //Assert
+            Assert.AreEqual(quiz.English, ENGLISH);
+            Assert.AreEqual(quiz.Czech, CZECH);
+        }
+
+        [TestMethod]
+        public void TestValidation()
+        {
+            //Arrange
+            var quiz = new WordQuiz();
+            quiz.Czech = CZECH;
+            quiz.English = ENGLISH;
+
+            //Assert (wrong translation yields validation errors)
+            quiz.userTranslation = ENGLISH;
+            var validationErrors = quiz.Validate(null);
+            Assert.IsFalse(validationErrors.ToList ().Count == 0);
+
+            //Assert (right translation yields no validation errors)
+            quiz.userTranslation = CZECH;
+            validationErrors = quiz.Validate(null);
+            Assert.IsTrue(validationErrors.ToList().Count == 0);
+
+        }
+
+    }
+}
